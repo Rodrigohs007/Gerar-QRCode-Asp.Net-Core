b617c92ef643926ada6c216cba5b330de2e50e0c
Rodrigo Henrique da Silva <rodrigohs007@outlook.com>
2022-06-20 21:44:06 -0300

    Publicando as Altetaçoes

---
+++ Gerando_QRCode/Gerando_QRCode/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Gerando_QRCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gerando_QRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string Caminho)
+        {
+
+            if (!String.IsNullOrEmpty(Caminho))
+            {
+                var byteArray = Funcoes.QRCode_Generate.GenerateByteArray(Caminho);
+                string strBase64 = Convert.ToBase64String(byteArray);
+
+                ViewBag.Base64String = "data:image/png;base64," + Convert.ToBase64String(byteArray, 0, byteArray.Length);
+            }
+            
+            return View();
+        }
+
+
+      
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Gerando_QRCode/Gerando_QRCode/Funcoes/QRCode_Generate.cs
@@ -0,0 +1,33 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace Gerando_QRCode.Funcoes
+{
+    public class QRCode_Generate
+    {
+
+        public static Bitmap GenerateImage(string url)
+        {
+            var qrGenerate = new QRCodeGenerator();
+            var qrCodeData = qrGenerate.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            var qrcode = new QRCode(qrCodeData);
+            var qrCodeImage = qrcode.GetGraphic(10);
+            return qrCodeImage;
+
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateImage(url);
+            return ImageToByte(image);
+        }
+    }
+}
